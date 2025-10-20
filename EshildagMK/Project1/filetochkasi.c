de029cf88b7075933e3fb4bbb19e849037cf3b1d
Maxesh <maximus6110@gmail.com>
2025-10-20 13:54:09 +0300

    Soobshenie

---
+++ EshildagMK/Project1/filetochkasi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+	int a, b;
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	printf("ans+: %d", a + b);
+	printf("ans-: %d", a - b);
+	printf("ans*: %d", a * b);
+	printf("ans%: %d", a % b);
+	return 0;
+	//emk
+}
